6157506b6d6652cb1a2df04ec75609012ff67051
Jose Antonio <Jose Antonio@LAPTOP-7HGF03AI>
2020-10-01 20:10:38 +0200

    Commit

---
+++ tema1/src/main/java/es/iesalb/dam2/di/jasc/tema1/tema1/Ejercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.iesalb.dam2.di.jasc.tema1.tema1;
+
+/**
+ *
+ * @author Jose Antonio
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
